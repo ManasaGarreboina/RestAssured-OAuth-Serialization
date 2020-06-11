b1edef177aac5550fc8f88e51a224eaebe4711f5
ManasaGarreboina <manasa.g55@wipro.com>
2020-06-11 21:47:19 +0530

    Initial Commit

---
+++ src/test/java/org/test/restg/AppTest.java
@@ -0,0 +1,16 @@
+package org.test.restg;
+
+import org.testng.TestNG;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+    extends TestNG
+{
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+}
